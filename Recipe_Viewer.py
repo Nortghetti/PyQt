2a838ab78f07ad440f975dcc9c00072b77b3431f
Korgen <joshkal9876@hotmail.com>
2024-04-20 19:35:40 -0400

    Window Displays

--- Recipe_Viewer.py
+++ Recipe_Viewer.py
@@ -2,21 +2,33 @@ import sys
 import json
 from urllib.parse import urlparse
 from collections import namedtuple
+import re
 import requests
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import QPixmap
 
 
 class Recipe:
-    def __init__(self, name, cookTime, prepTime, recipeYield, ingredients, description, image_url):
+    def __init__(self, name, description, image, recipeYield, cookTime, prepTime, ingredients):
         self.name = name
-        self.cookTime = cookTime
-        self.prepTime = prepTime
+
+        cook_match = re.match(r'PT(\d+)M', cookTime)
+        if cook_match:
+            self.cookTime = int(cook_match.group(1))
+        else:
+            self.cookTime = 0
+
+        prep_match = re.match(r'PT(\d+)M', prepTime)
+        if prep_match:
+            self.prepTime = int(prep_match.group(1))
+        else:
+            self.prepTime = 0
+
         self.recipeYield = recipeYield
         self.ingredients = ingredients
         self.description = description
-        self.image_url = image_url
-        self.image_file = None
+        self.image_file = image
 
     # Get name of recipe
     def get_name(self):
@@ -45,6 +57,7 @@ class Recipe:
                     f.write(response.content)
         except Exception as e:
             print("Error while downloading image", e)
+        
 
     # Returns name of an image file for saving or displaying (saved images must have same name as url)
     def get_image(self):
@@ -75,37 +88,86 @@ class RecipeProcessor:
         return self.recipes
     
 class RecipeUi(QDialog):
-    def __init__(self, height, width):
+    def __init__(self, height, width, recipes):
         super().__init__()
         self.height = height
         self.width = width
+        self.recipes = recipes
+        self.page = 0
+        self.per_page = 4
         self.setup_window()
 
     def setup_window(self):
-        layout = QGridLayout()
-        recipe1 = QGroupBox()
-        recipe2 = QGroupBox()
-        recipe3 = QGroupBox()
-        recipe4 = QGroupBox()
-        layout.addWidget(recipe1(), 0,0)
-        layout.addWidget(recipe2(), 0,1)
-        layout.addWidget(recipe3(), 1,0)
-        layout.addWidget(recipe4(), 1,1)
         self.setWindowTitle("Recipes")
         self.setFixedSize(QSize(self.width, self.height))
-        self.setLayout(layout)
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
         self.layout_ui()
 
-    def layout_ui(self, recipes:list):
-        count = recipes.len()
-        
-        
+    def layout_ui(self):
+        start_index = self.page * self.per_page
+        end_index = min((self.page + 1) * self.per_page, len(self.recipes))
+
+        for i, recipe_index in enumerate(range(start_index, end_index)):
+            recipe = self.recipes[recipe_index]
+            group_box = QGroupBox()
+            group_box.setFixedSize(QSize(int(self.width/2), int(self.height/2)))
+            group_layout = QVBoxLayout(group_box)
+            number_label = QLabel(f"Recipe #: {recipe_index}")
+            name_label = QLabel(f"Name: {recipe.get_name()}")
+            cook_label = QLabel(f"Cook Time: {recipe.get_cook_time()}")
+            prep_label = QLabel(f"Prep Time: {recipe.get_prep_time()}")
+
+            view_button = QPushButton("View Recipe")
+            #view_button.clicked.connect() #show the recipe
+
+            #donwload images
+            recipe.set_image(recipe.get_image())
+
+            image_label = QLabel()
+            pixmap = QPixmap(recipe.get_image())
+            image_label.setPixmap(pixmap.scaledToWidth(int(self.width/2)))
+
+            group_layout.addWidget(image_label)
+
+            group_layout.addWidget(number_label)
+            group_layout.addWidget(name_label)
+            group_layout.addWidget(cook_label)
+            group_layout.addWidget(prep_label)
+            group_layout.addWidget(view_button)
+            self.layout.addWidget(group_box, i//2, i%2)
+
+    def next():
+        ###called when the user clicks the next button. This method should get the next set of recipes in the list and update the UI with new information
+        ()
+    def previous():
+        ###called when the user clicks the previous button. This method should get the previous set of recipes in the list and update the UI with new information
+        ()
+    def first():
+        ### jumps to the beginning of the list of recipes and shows the first set in the UI based on the selected numberper page value
+        ()
+    def last():
+         ###jumps to the end of the list of recipes and shows the last set in the UI based on the selected number per page value
+        ()
+    def search(recipe_keywords):
+        ###searches the recipe list for recipes whose name, ingredients, or description contains the user-supplied string. The next, previous, first, and last buttons should also be used to navigate the search results when the user submits a search query.
+        ()
+    def reset():
+        ###clears the search results and returns to normal pagination of the list of recipes
+        ()
+    
 
 
 
 def main():
     processor = RecipeProcessor()
     processor.load_recipes('recipes.json')
+
+    app = QApplication(sys.argv)
+    recipes = processor.get_recipes()
+    window = RecipeUi(400, 600, recipes)
+    window.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
